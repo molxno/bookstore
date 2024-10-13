52634795840f8e6500df8cc4f4b05c3d116d7da7
MolanoSantiago <s.molano0818@gmail.com>
2024-10-13 14:15:54 -0500

    rename project

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_shop project.
+URL configuration for bookstore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
