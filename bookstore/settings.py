d23ede4e87b523012351ecbd8d40eebfbdae35cd
MolanoAlternova <santiago.molano@alternova.com>
2024-11-16 22:40:19 -0500

    setup env

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME', 'bookstore'),
+        'USER': os.getenv('DB_USERNAME', 'santiago'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'santiago'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
